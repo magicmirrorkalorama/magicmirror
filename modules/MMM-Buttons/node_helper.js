211406e9e80a2bfe631890e18f93a81d411ae0e3
Michael Scott Hertzberg <mshertzberg@gmail.com>
2020-05-10 22:41:05 -0400

    update module

--- modules/MMM-Buttons/node_helper.js
+++ modules/MMM-Buttons/node_helper.js
@@ -72,7 +72,8 @@ module.exports = NodeHelper.create({
 
     var options = {
       persistentWatch: true,
-      activeLow: !!self.buttons[index].activeLow
+      activeLow: !!self.buttons[index].activeLow,
+      debounceTimeout: 500
     }
 
     var pir = new Gpio(self.buttons[index].pin, 'in', 'both', options)
