4ebe5023b6a06436b066569e700185f9e3a8c4e2
Michael Scott Hertzberg <mshertzberg@gmail.com>
2020-05-16 13:34:58 -0400

    update pin notifications

--- modules/joystick/joystick.js
+++ modules/joystick/joystick.js
@@ -12,13 +12,13 @@ Module.register('joystick', {
   },
 
   start: function () {
-    Log.debug('Starting module: ' + this.name)
+    Log.info('Starting module: ' + this.name)
     this.scheduleUpdateInterval()
   },
 
   socketNotificationReceived: function (notification, payload, sender) {
     if (notification === 'ALL_MODULES_STARTED') {
-      Log.debug(
+      Log.info(
         this.name +
           ' received a socket notification: ' +
           notification +
@@ -27,20 +27,20 @@ Module.register('joystick', {
       )
 
       // if (notification === 'PIN_CONFIG') {
-      //   Log.debug('PIN_CONFIG' + JSON.stringify(payload))
+      //   Log.info('PIN_CONFIG' + JSON.stringify(payload))
       // }
 
       // if (notification === 'TOGGLE_PIN') {
-      //   Log.debug('TOGGLE_PIN' + JSON.stringify(payload))
+      //   Log.info('TOGGLE_PIN' + JSON.stringify(payload))
       // }
 
       // if (notification === 'JOYSTICK_POSITION') {
-      //   Log.debug('JOYSTICK_POSITION' + JSON.stringify(payload))
+      //   Log.info('JOYSTICK_POSITION' + JSON.stringify(payload))
       //   // this.updatePosition(payload)
       // }
 
       // if (notification === 'BUTTON_PRESS') {
-      //   Log.debug('BUTTON_PRESS' + JSON.stringify(payload))
+      //   Log.info('BUTTON_PRESS' + JSON.stringify(payload))
       //   // this.updateButton(payload)
       // }
     }
@@ -52,12 +52,12 @@ Module.register('joystick', {
   },
 
   updatePosition: function (payload) {
-    Log.debug('POSITION CHANGED --', payload)
+    Log.info('POSITION CHANGED --', payload)
     this.config.position = payload
   },
 
   updateButton: function (payload) {
-    Log.debug('BUTTON STATUS CHANGED --', payload)
+    Log.info('BUTTON STATUS CHANGED --', payload)
     this.config.button = payload
   },
 
